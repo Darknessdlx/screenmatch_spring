a9804b743a48e793e786d69b8ed5647db7867212
BryanBlue <darknessdlx@gmail.com>
2025-07-13 00:26:45 -0600

    Final 12-07

---
+++ src/main/java/com/nexxxt/screenmatch_spring/model/DatosEpisodio.java
@@ -0,0 +1,12 @@
+package com.nexxxt.screenmatch_spring.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties
+public record DatosEpisodio(
+        @JsonAlias("Title") String titulo,
+        @JsonAlias("Episode")Integer numeroEpisodio,
+        @JsonAlias("imdbRating")String evaluacion,
+        @JsonAlias("Released")String fechaDeLanzamiento) {
+}
