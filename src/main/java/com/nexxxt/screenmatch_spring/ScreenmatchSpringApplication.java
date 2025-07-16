a6a38177ada17bc3bcd63d1d1640ae7dbea0bd48
BryanBlue <darknessdlx@gmail.com>
2025-07-15 19:56:30 -0600

    Inicio 15-07

--- src/main/java/com/nexxxt/screenmatch_spring/ScreenmatchSpringApplication.java
+++ src/main/java/com/nexxxt/screenmatch_spring/ScreenmatchSpringApplication.java
@@ -1,5 +1,6 @@
 package com.nexxxt.screenmatch_spring;
 
+import com.nexxxt.screenmatch_spring.main.Main;
 import com.nexxxt.screenmatch_spring.model.DatosEpisodio;
 import com.nexxxt.screenmatch_spring.model.DatosSerie;
 import com.nexxxt.screenmatch_spring.model.DatosTemporadas;
@@ -9,6 +10,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,27 +23,8 @@ public class ScreenmatchSpringApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        var consumoApi = new ConsumoAPI();
-        var json = consumoApi.obtenerDatos("http://www.omdbapi.com/?t=game+of+thrones&apikey=d4d0bf92");
+        Main principal = new Main();
+        principal.muestraMenu();
 
-        System.out.println(json);
-
-        ConvierteDatos conversor = new ConvierteDatos();
-        var datos = conversor.obtenerDatos(json, DatosSerie.class);
-
-        System.out.println(datos);
-
-        json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=game+of+thrones&Season=1&Episode=1&apikey=d4d0bf92");
-        DatosEpisodio episodio = conversor.obtenerDatos(json, DatosEpisodio.class);
-
-        System.out.println(episodio);
-
-        List<DatosTemporadas> temporadas = new ArrayList<>();
-        for (int i = 1; i <= datos.totalDeTemporadas(); i++){
-            json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=game+of+thrones&Season=" + i + "&apikey=d4d0bf92");
-            var datosTemporadas = conversor.obtenerDatos(json, DatosTemporadas.class);
-            temporadas.add(datosTemporadas);
-        }
-        temporadas.forEach(System.out::println);
     }
 }
